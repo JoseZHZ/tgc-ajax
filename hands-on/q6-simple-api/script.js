0a2e1428c378c82be5318ea634f3a73037a33e0d
JoseZHZ <jose13467958@gmail.com>
2022-05-26 09:26:31 +0000

    q6 done

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,2 +1,5 @@
-
 let url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+document.querySelector('#load-btn').addEventListener('click', async function() {
+    let response = await axios.get(url)
+    document.querySelector('#quote').innerText = response.data.en
+})
